5678f6d5e230db7f8efaa6a7f3694e01e174168f
Josh <jsig885@gmail.com>
2024-03-03 15:47:10 -0600

    mar3 - Game
    - files: EndSystemObject.js and RouterObject.js currently in GameScene.js
    - GameObject.js has been abandoned for now

--- game/src/objects/RouterObject.js
+++ game/src/objects/RouterObject.js
@@ -1,62 +1,35 @@
-class RouterObject extends Phaser.GameObjects.Container {
-    constructor(scene, x, y) {
+class RouterObject extends Phaser.GameObjects.GameObject {
+
+    constructor(scene, x, y, name) {
         super(scene, x, y);
-
-        // Load the router image
-        scene.load.image('router1', '/assets/images/router1.png');
-
         // Create the router sprite
-        const router = scene.add.sprite(350, 400, 'router').setDisplaySize(150, 150).setInteractive({ pixelPerfect: true, draggable: true });
-
-        // Enable input for the router object;
-        router.on('drag', function (pointer, dragX, dragY) {
-            this.x = dragX;
-            this.y = dragY;
+        this.name = name;
+        scene.load.image('router', 'assets/images/router.png');
+        const object = scene.add.sprite(x, y, 'router').setSize(150, 150).setInteractive({ pixelPerfect: true, draggable: true });
+    
+        object.on('drag', function (pointer, dragX, dragY) {
+            x = dragX;
+            y = dragY;
+            this.x = x;
+            this.y = y;
         });
 
-        // Add event listeners for mouse interactions
-        this.on('pointerdown', this.onPointerDown, this);
-        this.on('pointerup', this.onPointerUp, this);
-        this.on('pointermove', this.onPointerMove, this);
+        object.on('pointerdown', function (pointer, x,y, event) {
+            // check if the pointer is over a object
+            // if it is, select the object
 
-        // Initialize properties for mouse interactions
-        this.isDragging = false;
-        this.isSelected = false;
-        this.startX = x;
-        this.startY = y;
+            console.log(`${name} clicked`);
+            return name;
+        });
+    
     }
 
-    onPointerDown(pointer) {
-        // Set the dragging flag to true
-        this.isDragging = true;
-        this.isSelected = true;
-
-        if (this.isSelected) {
-            this.scene.children.bringToTop(this);
-            this.setTint(0x00ff00);
-        }
-
-        // Store the initial position of the router object
-        this.startX = this.x;
-        this.startY = this.y;
-
-        // Bring the router object to the top of the display list
-        this.scene.children.bringToTop(this);
+    getName() {
+        return this.name;
     }
 
-    onPointerUp(pointer) {
-        // Set the dragging flag to false
-        this.isDragging = false;
+    getCoordinates() {
+        return { x: this.x, y: this.y };
     }
 
-    onPointerMove(pointer) {
-        // Check if the router object is being dragged
-        if (this.isDragging) {
-            // Calculate the new position based on the pointer movement
-            const deltaX = pointer.x - pointer.prevPosition.x;
-            const deltaY = pointer.y - pointer.prevPosition.y;
-            this.x += deltaX;
-            this.y += deltaY;
-        }
-    }
 }
