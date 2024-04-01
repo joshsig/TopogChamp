87e3006c2aa7b76401bf79d58a7b9adefdcb75f0
Josh <jsig885@gmail.com>
2024-03-31 19:51:37 -0500

    Update josh-dev

--- testing/main.js
+++ testing/main.js
@@ -18,10 +18,19 @@ fetch('/levelsData.json')
 var network = null;
 var selectedData = null;
 
+function resetData() {
+    let curr_level = document.getElementById("levelSelect").value;
+    updateData(curr_level);
+}
+
 // Function to update the nodes and edges based on the selected level
 function updateData(selectedLevel) {
     selectedData = levelsData[selectedLevel];
     if (selectedData) {
+
+        let goal = selectedData.goal;
+        let goalText = document.getElementById("goal");
+        goalText.innerHTML = goal;
         
         var container = document.getElementById("mynetwork");
         // Update nodes and edges arrays based on selected level
@@ -38,13 +47,16 @@ function updateData(selectedLevel) {
           arrows = selectedData.options.arrows;
           manipulation = selectedData.options.manipulation;
         } catch (e) {
-          arrows = true
+          arrows = false
           manipulation = true
         }
 
         var options = {
-            manipulation: Boolean(manipulation),
-            edges: { arrows: { to: {enabled: Boolean(arrows)} } }
+            manipulation: Boolean(manipulation), 
+            edges: { arrows: { to: {enabled: false} } },
+            physics: {
+                enabled: false,
+            },
         };
         network = new vis.Network(container, data, options);
     } else {
@@ -67,20 +79,126 @@ function submitAnswer() {
     let nodes = []
     let curr_edges = []
     for (var i = 0; i < selectedData.nodes.length; i++) {
-        console.log(selectedData.nodes[i].id);
         nodes.push(selectedData.nodes[i].id);
     }
+    let curr_nodes = network.selectNodes(nodes); 
     for (var i = 0; i < nodes.length; i++) {
         curr_edges.push(network.getConnectedEdges(nodes[i]));
     }
     
-    // check if the answer is correct
-    if (curr_edges.length === 5) {
-        
-        alert("Correct Answer!");
-    } else {
-        alert("Incorrect Answer!");
+    // switch statement for each level
+    let curr_level = document.getElementById("levelSelect").value;
+    let correct = false;
+
+    switch(curr_level) {
+        case "ring":
+            if (ringCheck(curr_edges, nodes)) {
+                correct = true;
+                console.log("Correct answer!")
+            } else {
+                console.log("Incorrect answer.")
+            }
+            break;
+        case "star":
+            if (starCheck(curr_edges, nodes)) {
+                correct = true;
+                console.log("Correct answer!")
+            } else {
+                console.log("Incorrect answer.")
+            }
+            break;
+        case "bus":
+            if (busCheck(curr_edges, nodes)) {
+                correct = true;
+            }
+            break;
+        case "fully_connected":
+          if (fullyConnectedCheck(curr_edges, nodes)) {
+            correct = true;
+            console.log("Correct answer!")
+        } else {
+            console.log("Incorrect answer.")
+        }
+        break;
+        case "partial_connected":
+            if (partialCheck(curr_edges, nodes)) {
+              correct = true;
+              console.log("Correct answer!")
+            } else {
+                console.log("Incorrect answer.")
+            }
+            break;
+        default:
+            console.error("Invalid level selected.");
+            break;
     }
 }
 
 
+function ringCheck(edges, nodes) {
+    // check if the edges are correct
+    // if each node has 2 edges, then it is a ring
+    for (var i = 0; i < edges.length; i++) {
+        if (edges[i].length != 2) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function starCheck(edges, nodes) {
+    // check if the edges are correct
+    // if one node has n-1 edges, and the rest have 1 edge, then it is a star
+    console.log(nodes)
+    let n = nodes.length;
+    let center = -1;
+    for (var i = 0; i < edges.length; i++) {
+        if (edges[i].length == n-1) {
+            if (center != -1) {
+                return false;
+            }
+            center = i;
+        } else if (edges[i].length != 1) {
+            return false;
+        }
+        if (center >= 0) {
+          if (!nodes[center].startsWith("r")) {
+              return false;
+        }
+      }
+    }
+    return true;
+}
+
+function busCheck(edges, nodes) {
+    // check if the edges are correct
+    // if each node has 1 edge, then it is a bus
+    for (var i = 0; i < edges.length; i++) {
+        if (edges[i].length != 1) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function fullyConnectedCheck(edges, nodes) {
+   // check if every node has n-1 edges
+    let n = nodes.length;
+    for (var i = 0; i < edges.length; i++) {
+        if (edges[i].length != n-1) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function partialCheck(edges, nodes) {
+    // check if the edges are correct
+    // if each node has at least 1 edge, then it is a partially connected graph
+    for (var i = 0; i < edges.length; i++) {
+        if (edges[i].length < 1) {
+            return false;
+        }
+    }
+    return true;
+}
